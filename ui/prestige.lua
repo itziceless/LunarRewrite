619b91233c1b18339b06d7d6524267773d3c1a40
Iceless <xiceless@gmail.com>
2025-02-24 17:44:29 -0500

    Update prestige.lua

--- ui/prestige.lua
+++ ui/prestige.lua
@@ -1,1 +1,31 @@
---idk
+local tweenService = cloneref(game:GetService('TweenService'))
+local inputService = cloneref(game:GetService('UserInputService'))
+local textService = cloneref(game:GetService('TextService'))
+local guiService = cloneref(game:GetService('GuiService'))
+local runService = cloneref(game:GetService('RunService'))
+local httpService = cloneref(game:GetService('HttpService'))
+
+local fontsize = Instance.new('GetTextBoundsParams')
+fontsize.Width = math.huge
+local notifications
+local assetfunction = getcustomasset
+local getcustomasset
+local clickgui
+local scaledgui
+local toolblur
+local tooltip
+local scale
+local gui
+
+local color = {}
+local tween = {
+	tweens = {},
+	tweenstwo = {}
+}
+local uipallet = {
+	Main = Color3.fromRGB(26, 25, 26),
+	Text = Color3.fromRGB(200, 200, 200),
+	Font = Font.fromEnum(Enum.Font.Arial),
+	FontSemiBold = Font.fromEnum(Enum.Font.Arial, Enum.FontWeight.SemiBold),
+	Tween = TweenInfo.new(0.16, Enum.EasingStyle.Linear)
+}
