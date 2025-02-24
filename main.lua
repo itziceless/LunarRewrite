3a2fc30bb819cafc3acff6a7747b4e1fa31bb0db
Iceless <xiceless@gmail.com>
2025-02-24 14:52:57 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,10 +13,14 @@ local function wipeFolder(path)
 	if not isfolder(path) then return end
 	for _, file in listfiles(path) do
 		if file:find('loader') then continue end
-		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
+		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after lunar updates.')) == 1 then
 			delfile(file)
 		end
 	end
+end
+
+local function getid(placeid)
+	return game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/games/'..placeid..".lua", true)
 end
 
 for _, folder in {'lunar', 'lunar/games', 'lunar/uiassets', 'lunar/libs', 'lunar/ui'} do
@@ -26,5 +30,5 @@ for _, folder in {'lunar', 'lunar/games', 'lunar/uiassets', 'lunar/libs', 'lunar
 end
 
 if not isfile('lunar/games/'..game.PlaceId..'.lua') then
-	writefile('newvape/games/'..game.PlaceId..'.lua'), tostring(game.PlaceId)(...)
+	writefile('newvape/games/'..game.PlaceId..'.lua')
 end
