e2121d20c7dba261b68036912109cf36282c56a5
Iceless <xiceless@gmail.com>
2025-02-24 14:32:14 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,11 @@
 repeat task.wait() until game:IsLoaded()
-
-
+local isfile = isfile or function(file)
+	local suc, res = pcall(function()
+		return readfile(file)
+	end)
+	return suc and res ~= nil and res ~= ''
+end
+local delfile = delfile or function(file)
+	  writefile(file, '')
+  end
+end
