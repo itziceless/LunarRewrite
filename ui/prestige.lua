9acd047106d61f677f543ac025ef3bea884fbd8b
Iceless <xiceless@gmail.com>
2025-02-26 20:30:36 -0500

    Update prestige.lua

--- ui/prestige.lua
+++ ui/prestige.lua
@@ -1,3 +1,29 @@
+local mainapi = {
+	Categories = {},
+	GUIColor = {
+		Hue = 0.46,
+		Sat = 0.96,
+		Value = 0.52
+	},
+	HeldKeybinds = {},
+	Keybind = {'RightShift'},
+	Loaded = false,
+	Libraries = {},
+	Modules = {},
+	Place = game.PlaceId,
+	Profile = 'Lunar',
+	Profiles = {},
+	RainbowSpeed = {Value = 1},
+	RainbowUpdateSpeed = {Value = 60},
+	RainbowTable = {},
+	Scale = {Value = 1},
+	ThreadFix = setthreadidentity and true or false,
+	ToggleNotifications = {},
+	BlatantMode = {},
+	Version = 'V2',
+	Windows = {}
+}
+
 local tweenService = cloneref(game:GetService('TweenService'))
 local inputService = cloneref(game:GetService('UserInputService'))
 local textService = cloneref(game:GetService('TextService'))
@@ -55,3 +81,87 @@ uiassets = not inputService.TouchEnabled and assetfunction and function(path)
 end or function(path)
 	return getcustomassets[path] or ''
 end
+
+function mainapi:CreateCategory(categorysettings)
+	local categoryapi = {
+		Type = 'Category',
+		Expanded = false
+	}
+
+	local window = Instance.new('TextButton')
+	window.Name = categorysettings.Name..'Category'
+	window.Size = UDim2.fromOffset(220, 41)
+	window.Position = UDim2.fromOffset(236, 60)
+	window.BackgroundColor3 = uipallet.Main
+	window.AutoButtonColor = false
+	window.Visible = false
+	window.Text = ''
+	window.Parent = clickgui
+	addBlur(window)
+	addCorner(window)
+	makeDraggable(window)
+	local icon = Instance.new('ImageLabel')
+	icon.Name = 'Icon'
+	icon.Size = categorysettings.Size
+	icon.Position = UDim2.fromOffset(12, (icon.Size.X.Offset > 20 and 14 or 13))
+	icon.BackgroundTransparency = 1
+	icon.Image = categorysettings.Icon
+	icon.ImageColor3 = uipallet.Text
+	icon.Parent = window
+	local title = Instance.new('TextLabel')
+	title.Name = 'Title'
+	title.Size = UDim2.new(1, -(categorysettings.Size.X.Offset > 18 and 40 or 33), 0, 41)
+	title.Position = UDim2.fromOffset(math.abs(title.Size.X.Offset), 0)
+	title.BackgroundTransparency = 1
+	title.Text = categorysettings.Name
+	title.TextXAlignment = Enum.TextXAlignment.Left
+	title.TextColor3 = uipallet.Text
+	title.TextSize = 13
+	title.FontFace = uipallet.Font
+	title.Parent = window
+	local arrowbutton = Instance.new('TextButton')
+	arrowbutton.Name = 'Arrow'
+	arrowbutton.Size = UDim2.fromOffset(40, 40)
+	arrowbutton.Position = UDim2.new(1, -40, 0, 0)
+	arrowbutton.BackgroundTransparency = 1
+	arrowbutton.Text = ''
+	arrowbutton.Parent = window
+	local arrow = Instance.new('ImageLabel')
+	arrow.Name = 'Arrow'
+	arrow.Size = UDim2.fromOffset(9, 4)
+	arrow.Position = UDim2.fromOffset(20, 18)
+	arrow.BackgroundTransparency = 1
+	arrow.Image = getcustomasset('newvape/assets/new/expandup.png')
+	arrow.ImageColor3 = Color3.fromRGB(140, 140, 140)
+	arrow.Rotation = 180
+	arrow.Parent = arrowbutton
+	local children = Instance.new('ScrollingFrame')
+	children.Name = 'Children'
+	children.Size = UDim2.new(1, 0, 1, -41)
+	children.Position = UDim2.fromOffset(0, 37)
+	children.BackgroundTransparency = 1
+	children.BorderSizePixel = 0
+	children.Visible = false
+	children.ScrollBarThickness = 2
+	children.ScrollBarImageTransparency = 0.75
+	children.CanvasSize = UDim2.new()
+	children.Parent = window
+	local divider = Instance.new('Frame')
+	divider.Name = 'Divider'
+	divider.Size = UDim2.new(1, 0, 0, 1)
+	divider.Position = UDim2.fromOffset(0, 37)
+	divider.BackgroundColor3 = Color3.new(1, 1, 1)
+	divider.BackgroundTransparency = 0.928
+	divider.BorderSizePixel = 0
+	divider.Visible = false
+	divider.Parent = window
+	local windowlist = Instance.new('UIListLayout')
+	windowlist.SortOrder = Enum.SortOrder.LayoutOrder
+	windowlist.HorizontalAlignment = Enum.HorizontalAlignment.Center
+	windowlist.Parent = children
+
+	mainapi:CreateCategory({
+	Name = 'Combat',
+	Icon = nil
+	Size = UDim2.fromOffset(13, 14)
+})
