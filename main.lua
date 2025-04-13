47f84f2fbec7d032e26f01e1ca48423ab4cc792b
Iceless <xiceless@gmail.com>
2025-04-13 00:01:21 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 
+local DeveloperMode = getgenv().Developer or false
+
 local isfile = isfile or function(file)
     local suc, res = pcall(function()
         return readfile(file)
@@ -21,68 +23,19 @@ local function wipeFolder(path)
     end
 end
 
-local function downloadFile(path, func)
-	if not isfile(path) then
-		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/itziceless/Lunar/'..readfile('lunar/libs/anticache.txt')..'/'..select(1, path:gsub('lunar/', '')), true)
-		end)
-		if not suc or res == '404: Not Found' then
-			error(res)
-		end
-		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after lunar updates.\n'..res
-		end
-		writefile(path, res)
-	end
-	return (func or readfile)(path)
-end
-
-local function getid(placeid)
-    return game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/games/'..placeid..".lua", true)
-end
-
-for _, folder in {'lunar', 'lunar/games', 'lunar/uiassets', 'lunar/libs', 'lunar/ui', 'lunar/config'} do
+for _, folder in {'ocean', 'ocean/games', 'ocean/uiassets', 'ocean/libs', 'ocean/ui', 'ocean/config'} do
     if not isfolder(folder) then
         makefolder(folder)
     end
 end
 
-if not isfile('lunar/ui/mode.txt') then
-    writefile('lunar/ui/mode.txt', 'prestig')
-end
-local uiasset = readfile('lunar/ui/mode.txt')
-
-if not isfolder('lunar/uiassets/'..uiasset) then
-    makefolder('lunar/uiassets/'..uiasset)
+if not isfile('ocean/libs/ui.txt') then
+    writefile('ocean/ui/ui.txt', loadstring(game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/libs/ui.lua'))())
 end
 
-local wltext = game:HttpGet("https://raw.githubusercontent.com/itziceless/whitelists/refs/heads/main/PlayerWhitelist.json", true)
-
-if isfile('lunar/libs/wl.json') then
-	delfile('lunar/libs/wl.json')
-
-if not isfile('lunar/libs/wl.json') then
-	writefile('lunar/libs/wl.json', wltext)
+if not isfile('ocean/games/universal.txt') then
+    writefile('ocean/ui/universal.txt', loadstring(game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/games/universal.lua'))())
 end
 
-local success, subbed = pcall(function()
-    return game:HttpGet("https://api.github.com/repos/itziceless/LunarRewrite/git/refs/heads/main", true)
-end)
-
-if success and subbed then
-    local commit = subbed:find('currentOid')
-    commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-    commit = commit and #commit == 40 and commit or 'main'
-
-    if commit == 'main' or (isfile('lunar/libs/anticache.txt') and readfile('lunar/libs/anticache.txt') or '') ~= commit then
-        wipeFolder('lunar')
-        wipeFolder('lunar/games')
-        wipeFolder('lunar/ui')
-        wipeFolder('lunar/libs')
-    end
-    writefile('lunar/libs/anticache.txt', commit)
-    end
-
-	getid()
-	loadstring(game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/ui/prestige.lua'))()
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/libs/ui.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/itziceless/LunarRewrite/refs/heads/main/games/universal.lua'))()
